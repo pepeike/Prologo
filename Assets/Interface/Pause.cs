a35b0dba17bdf06907e7c285a7dd43d284e48344
Pepper Ikeoka <pedroikeoka@gmail.com>
2025-09-19 23:38:20 -0300

    pause fix

--- Assets/Interface/Pause.cs
+++ Assets/Interface/Pause.cs
@@ -1,52 +1,60 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
-public class PauseMenu : MonoBehaviour
-{
+public class PauseMenu : MonoBehaviour {
     public GameObject pauseMenuUI; // Arraste aqui o painel do menu de pause no Inspector
     private bool isPaused = false;
+    InputAction inputAction;
 
-    void Start()
-    {
+    
+
+    void Start() {
         // Garantir que o menu esteja invisível no início
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
     }
 
-    void Update()
-    {
-        // Detecta o pressionar da tecla ESC para pausar/despausar
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (isPaused)
+    public void TogglePause(InputAction.CallbackContext context) {
+        if (context.started) {
+            if (isPaused) {
                 Resume();
-            else
+            } else {
                 Pause();
+            }
         }
     }
 
-    public void Resume()
-    {
+    void Update() {
+        // Detecta o pressionar da tecla ESC para pausar/despausar
+
+        
+
+    }
+
+    public void Resume() {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f; // volta o tempo normal
         isPaused = false;
     }
 
-    public void Pause()
-    {
+    public void Pause() {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f; // pausa o tempo
         isPaused = true;
     }
 
-    public void LoadMainMenu()
-    {
+    public void LoadMainMenu() {
         Time.timeScale = 1f; // despausa antes de carregar a cena
         SceneManager.LoadScene("MainMenu"); // substitua pelo nome correto da sua cena
     }
 
-    public void QuitGame()
-    {
+    public void QuitGame() {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
+
 }
